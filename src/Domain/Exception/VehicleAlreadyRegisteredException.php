cf7c3b0e3540d5729e62c32a3c9939bb4cf300ec
tomJM <tom@tomweb.fr>
2025-10-23 15:36:46 +0400

    Change Domain/Exception extends to \DomainException

--- src/Domain/Exception/VehicleAlreadyRegisteredException.php
+++ src/Domain/Exception/VehicleAlreadyRegisteredException.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace Fulll\Domain\Exception;
 
-final class VehicleAlreadyRegisteredException extends \RuntimeException
+final class VehicleAlreadyRegisteredException extends \DomainException
 {
 }
